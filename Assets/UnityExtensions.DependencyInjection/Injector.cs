e17fc993c85db1428d7c134d5a4e3dafff1317e8
Kurai András <k.andras9642@gmail.com>
2020-03-28 13:20:19 +0100

    Add project

---
+++ Assets/UnityExtensions.DependencyInjection/Injector.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using UnityEngine;
+
+namespace UnityExtensions.DependencyInjection
+{
+    public abstract class Injector : MonoBehaviour
+    {
+        public abstract void AddInjections(IServiceCollection services);
+    }
+}
