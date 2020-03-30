50ff5cf3e4942848672dead784d9640897deee98
Kurai András <k.andras9642@gmail.com>
2020-03-30 17:13:38 +0200

    Change injector

--- Assets/UnityExtensions.DependencyInjection/Injector.cs
+++ Assets/UnityExtensions.DependencyInjection/Injector.cs
@@ -5,36 +5,16 @@ using UnityExtensions.DependencyInjection.Extensions;
 
 namespace UnityExtensions.DependencyInjection
 {
-    /// <summary>
-    /// Has execution order : -999
-    /// </summary>
-    [DefaultExecutionOrder(-999)]
     [RequireComponent(typeof(SceneInjector))]
     public abstract class Injector : MonoBehaviour
     {
-        private static readonly IServiceCollection DefaultServiceCollection = new ServiceCollection().AddInstantiation();
+        protected SceneInjector SceneInjector => GetComponent<SceneInjector>();
+        protected IServiceCollection Services { get; set; }
 
-        protected Injector() => ServiceProvider = Services.BuildServiceProvider() ?? DefaultServiceCollection.BuildServiceProvider();
+        protected Injector() => Services = new ServiceCollection().AddInstantiation();
 
-        protected IServiceCollection Services { get; set; } = DefaultServiceCollection;
+        protected virtual IServiceProvider CreateServiceProvider() => Services.BuildServiceProvider();
 
-        /// <summary>
-        /// Set to initialize services
-        /// </summary>
-        protected IServiceProvider ServiceProvider { get; set; }
-
-        /// <summary>
-        /// Starts initializing the scene with the ServiceProvider.
-        /// Gets called in Awake.
-        /// Injector should be set as the first relevant executing script.
-        /// Override this and add services here.
-        /// Don't forget to call base when overriden.
-        /// </summary>
-        protected virtual void Startup() => GetComponent<SceneInjector>().InitializeScene(ServiceProvider);
-
-        /// <summary>
-        /// Calls Startup
-        /// </summary>
-        protected virtual void Awake() => Startup();
+        protected virtual void Awake() => SceneInjector.InitializeScene(CreateServiceProvider());
     }
 }
