2215166918d127fe01217fc4a956a51e7078c0e2
Kurai András <k.andras9642@gmail.com>
2020-03-30 17:36:06 +0200

    Update code summaries

--- Assets/UnityExtensions.DependencyInjection/Injector.cs
+++ Assets/UnityExtensions.DependencyInjection/Injector.cs
@@ -13,8 +13,16 @@ namespace UnityExtensions.DependencyInjection
 
         protected Injector() => Services = new ServiceCollection().AddInstantiation();
 
+        /// <summary>
+        /// Create service registration here.
+        /// Builds <see cref="IServiceProvider"/> from <see cref="Services"/>
+        /// </summary>
         protected virtual IServiceProvider CreateServiceProvider() => Services.BuildServiceProvider();
 
+        /// <summary>
+        /// Calls <see cref="SceneInjector"/> With <see cref="CreateServiceProvider"/>
+        /// Add <see cref="DefaultExecutionOrder"/> to ensure this awake is called first in your scene.
+        /// </summary>
         protected virtual void Awake() => SceneInjector.InitializeScene(CreateServiceProvider());
     }
 }
